ca67dac42e9debe10a6327576e6c1c0596e4992f
varun-coder-git <writetovarungupta@gmail.com>
2021-01-09 12:53:05 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SuperWorks!!-The WallMart';
+  title = 'SuperWorks!!';
  
 }
